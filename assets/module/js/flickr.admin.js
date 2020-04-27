b02cfb99822f3a01dc4acf036f09f0ad4d618549
Khai Huynh <huynhk@yorku.ca>
2020-04-27 00:17:45 -0400

    add gallery library setup gallery in flickr_album admin page

---
+++ assets/module/js/flickr.admin.js
@@ -0,0 +1,28 @@
+/**
+ * @file custom_module.js
+ * Custom Module main JavaScript file.
+ *
+ * You will need to get rid of the comments.
+ * For how to add this file to your module, see https://drupal.org/node/304255
+ */
+
+// The first and last lines are a closure that allow us to use the `$()`
+// shortcut, instead of the longer `jQuery()` function.
+(function ($) {
+
+    // Implementing a Drupal JavaScript behavior allow us to run code when the
+    // page is ready and every time elements are added via the Drupal AJAX API.
+    Drupal.behaviors.flickr = {
+        // The `context` and `settings` arguments are specific to when and where
+        // this is being executed.
+        attach: function (context, settings) {
+            $("#admin-photoset-gallery").lightGallery({
+                thumbnail: true,
+                selector: "a"
+            }).justifiedGallery({
+                "rowHeight": 150,
+            });
+
+        }
+    };
+})(jQuery);
